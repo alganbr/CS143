bdc3b56489d7bcdb649069104f07563a74815089
Frederick Kennedy <fkennedy@ucla.edu>
2017-04-29 01:01:05 -0700

    Added code
    Code clean up for add_actordirector.php
    Added PHP functionality to add_movie.php

--- Project 1B/www/add_actordirector.php
+++ Project 1B/www/add_actordirector.php
@@ -16,176 +16,173 @@
   <!-- Latest compiled and minified JavaScript -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </head>
-<body>
-   <!-- Navbar -->
+  <body>
+    <!-- Navbar -->
     <div class="navbar navbar-inverse navbar-static-top center" role="navigation">
-        <div class="container">
+      <div class="container">
 
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-              <a href="index.php">
-                <div class="navbar-brand">
-                  CS 143
-                  <small>Project 1B</small>
-                </div>
-              </a>
-          </div>
-
-          <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Add New Content<b class="caret"></b></a>
-                <ul class="dropdown-menu dropdown-menu-left">
-                  <li><a href="add_actordirector.php">Add Actor/Director</a></li>
-                  <li><a href="add_movie.php">Add Movie Information</a></li>
-                  <li><a href="add_movieactor.php">Add Movie/Actor Relation</a></li>
-                  <li><a href="add_moviedirector.php">Add Movie/Director Relation</a></li>
-                </ul>
-              </li>
-
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Browsing Content<b class="caret"></b></a>
-                <ul class="dropdown-menu dropdown-menu-left">
-                  <li><a href="show_actor.php">Show Actor Information</a></li>
-                  <li><a href="show_movie.php">Show Movie Information</a></li>
-                </ul>
-              </li>
-
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Search Interface<b class="caret"></b></a>
-                <ul class="dropdown-menu dropdown-menu-left">
-                  <li><a href="search.php">Search Actor/Movie</a></li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="index.php">
+            <div class="navbar-brand">
+              CS 143
+              <small>Project 1B</small>
+            </div>
+          </a>
         </div>
-      </div>
-  <!-- End Navbar -->
 
-  <!-- Section -->
-  <div class="container" style="margin-left: 0">
-    <div class="row">
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Add New Content<b class="caret"></b></a>
+              <ul class="dropdown-menu dropdown-menu-left">
+                <li><a href="add_actordirector.php">Add Actor/Director</a></li>
+                <li><a href="add_movie.php">Add Movie Information</a></li>
+                <li><a href="add_movieactor.php">Add Movie/Actor Relation</a></li>
+                <li><a href="add_moviedirector.php">Add Movie/Director Relation</a></li>
+              </ul>
+            </li>
 
-      <div class="col-sm-3 col-md-3 sidebar">
-        <ul class="nav nav-sidebar">
-            <h3>Add New Content</h3>
-            <li><a href="add_actordirector.php">Add Actor/Director</a></li>
-            <li><a href="add_movie.php">Add Movie Information</a></li>
-            <li><a href="add_movieactor.php">Add Movie/Actor Relation</a></li>
-            <li><a href="add_moviedirector.php">Add Movie/Director Relation</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Browsing Content<b class="caret"></b></a>
+              <ul class="dropdown-menu dropdown-menu-left">
+                <li><a href="show_actor.php">Show Actor Information</a></li>
+                <li><a href="show_movie.php">Show Movie Information</a></li>
+              </ul>
+            </li>
+
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Search Interface<b class="caret"></b></a>
+              <ul class="dropdown-menu dropdown-menu-left">
+                <li><a href="search.php">Search Actor/Movie</a></li>
+              </ul>
+            </li>
           </ul>
-
-          <ul class="nav nav-sidebar">
-            <h3>Browsing Content</h3>
-            <li><a href="show_actor.php">Show Actor Information</a></li>
-            <li><a href="show_movie.php">Show Movie Information</a></li>
-          </ul>
-
-          <ul class="nav nav-sidebar">
-            <h3>Search Interface</h3>
-            <li><a href="search.php">Search Actor/Movie</a></li>
-          </ul>
-      </div>
-
-      <div class="col-sm-9 col-md-9" style="padding-left: 5%">
-        <form action="add_actordirector.php" method="GET">
-          <h3>Add New Actor/Director</h3>
-          <label class="radio-inline">
-            <input type="radio" name="identity" value="Actor" checked="checked">Actor</input>
-          </label>
-          <label class="radio-inline">
-            <input type="radio" name="identity" value="Director">Director</input>
-          </label>
-          <div class="form-group">
-             <label for="first_name">First Name</label>
-            <input type="text" class="form-control" placeholder="Text Input" name="fname">
-          </div>
-          <div class="form-group">
-             <label for="last_name">Last Name</label>
-            <input type="text" class="form-control" placeholder="Text Input" name="lname">
-          </div>
-          <label class="radio-inline">
-            <input type="radio" name="sex" value="Male" checked="checked">Male</input>
-          </label>
-          <label class="radio-inline">
-            <input type="radio" name="sex" value="Female">Female</input>
-          </label>
-          <div class="form-group">
-             <label for="first_name">Date of Birth</label>
-            <input type="text" class="form-control" placeholder="Text Input" name="DOB">
-            <p>ie: 1997-05-05</p>
-          </div>
-          <div class="form-group">
-             <label for="last_name">Date of Death</label>
-            <input type="text" class="form-control" placeholder="Text Input" name="DOD">
-            <p>(leave blank if alive now)</p>
-          </div>
-          <button type="submit" class="btn btn-default" name="btnSubmit">Add</button>
-        </form>
-	<br>
-        <?php 
-          $db_connection = mysql_connect("localhost", "cs143", "");
-          mysql_select_db("CS143", $db_connection);
-	  
-	  // get maxperson id
-	  $rs = mysql_query("SELECT id FROM MaxPersonID", $db_connection);
-	  if(!$rs) {
-	   die("Query failed: " . mysql_error());
-	  }
-	  $row = mysql_fetch_row($rs);
-	  $MaxPersonID = $row[0];
-	   
-	  // create new unique ID
-	  $new_id = $MaxPersonID + 1;
-  
-	  // create insertion query
-          $identity = $_GET["identity"];
-          $sex = "\"" . $_GET["sex"] . "\"";
-	  $fname = "\"" . $_GET["fname"] . "\"";
-	  $lname = "\"" . $_GET["lname"] . "\"";
-	  $dob = "\"" . $_GET["DOB"] . "\"";
-	  $dod = "\"" . $_GET["DOD"] . "\"";
-	  if($dod == "\"\"") $dod = "null";
-	  $query = "INSERT INTO " . $identity . " VALUES(" . $new_id . "," . $lname . "," . $fname . "," . $sex . "," . $dob . "," . $dod . ")";
-
-	  // insert into databas
-	  if(isset($_GET["btnSubmit"])) {
-	   if(!$fname) {
-	    echo "Query failed: first name is empty.<br>";
-	   }
-	   if(!$lname) {
-	    echo "Query failed: last name is empty.<br>";
-	   }
-	   if(!$dob) {
-	    echo "Query failed: date of birth is empty.<br>";
-	   }
-	   if($fname && $lname && $dob) {
-	    $rs = mysql_query($query, $db_connection);
-	    if(!rs) {
-	     die("Query failed: " . mysql_error());
-	    }
-	    else {
-	     echo "Successfully insert into database.";
-
-	     // update maxperson id
-	     mysql_query("UPDATE MaxPersonID SET id=" . $new_id, $db_connection);
-	    }
-	  }
-          mysql_close($db_connection);
-	 }
-        ?>
+        </div>
+        
       </div>
     </div>
-  </div>
+    <!-- End Navbar -->
 
+    <!-- Section -->
+    <div class="container" style="margin-left: 0">
+      <div class="row">
 
-</body>
+        <div class="col-sm-3 col-md-3 sidebar">
+          <ul class="nav nav-sidebar">
+              <h3>Add New Content</h3>
+              <li><a href="add_actordirector.php">Add Actor/Director</a></li>
+              <li><a href="add_movie.php">Add Movie Information</a></li>
+              <li><a href="add_movieactor.php">Add Movie/Actor Relation</a></li>
+              <li><a href="add_moviedirector.php">Add Movie/Director Relation</a></li>
+            </ul>
 
+            <ul class="nav nav-sidebar">
+              <h3>Browsing Content</h3>
+              <li><a href="show_actor.php">Show Actor Information</a></li>
+              <li><a href="show_movie.php">Show Movie Information</a></li>
+            </ul>
+
+            <ul class="nav nav-sidebar">
+              <h3>Search Interface</h3>
+              <li><a href="search.php">Search Actor/Movie</a></li>
+            </ul>
+        </div>
+
+        <div class="col-sm-9 col-md-9" style="padding-left: 5%">
+          <form action="add_actordirector.php" method="GET">
+            <h3>Add New Actor/Director</h3>
+            <label class="radio-inline">
+              <input type="radio" name="identity" value="Actor" checked="checked">Actor</input>
+            </label>
+            <label class="radio-inline">
+              <input type="radio" name="identity" value="Director">Director</input>
+            </label>
+            <div class="form-group">
+               <label for="first_name">First Name</label>
+              <input type="text" class="form-control" placeholder="Text Input" name="fname">
+            </div>
+            <div class="form-group">
+               <label for="last_name">Last Name</label>
+              <input type="text" class="form-control" placeholder="Text Input" name="lname">
+            </div>
+            <label class="radio-inline">
+              <input type="radio" name="sex" value="Male" checked="checked">Male</input>
+            </label>
+            <label class="radio-inline">
+              <input type="radio" name="sex" value="Female">Female</input>
+            </label>
+            <div class="form-group">
+               <label for="first_name">Date of Birth</label>
+              <input type="text" class="form-control" placeholder="Text Input" name="DOB">
+              <p>ie: 1997-05-05</p>
+            </div>
+            <div class="form-group">
+               <label for="last_name">Date of Death</label>
+              <input type="text" class="form-control" placeholder="Text Input" name="DOD">
+              <p>(leave blank if alive now)</p>
+            </div>
+            <button type="submit" class="btn btn-default" name="btnSubmit">Add</button>
+          </form>
+          <br>
+          <?php 
+            $db_connection = mysql_connect("localhost", "cs143", "");
+            mysql_select_db("CS143", $db_connection);
+      
+            // get maxperson id
+            $rs = mysql_query("SELECT id FROM MaxPersonID", $db_connection);
+            if(!$rs) {
+              die("Query failed: " . mysql_error());
+            }
+            $row = mysql_fetch_row($rs);
+            $MaxPersonID = $row[0];
+       
+            // create new unique ID
+            $new_id = $MaxPersonID + 1;
+    
+            // create insertion query
+            $identity = $_GET["identity"];
+            $sex = "\"" . $_GET["sex"] . "\"";
+            $fname = "\"" . $_GET["fname"] . "\"";
+            $lname = "\"" . $_GET["lname"] . "\"";
+            $dob = "\"" . $_GET["DOB"] . "\"";
+            $dod = "\"" . $_GET["DOD"] . "\"";
+            if($dod == "\"\"") $dod = "null";
+            $query = "INSERT INTO " . $identity . " VALUES(" . $new_id . "," . $lname . "," . $fname . "," . $sex . "," . $dob . "," . $dod . ")";
+
+            // insert into database
+            if(isset($_GET["btnSubmit"])) {
+              if(!$fname) {
+                echo "Query failed: first name is empty.<br>";
+              }
+              if(!$lname) {
+                echo "Query failed: last name is empty.<br>";
+              }
+              if(!$dob) {
+                echo "Query failed: date of birth is empty.<br>";
+              }
+              if($fname && $lname && $dob) {
+                $rs = mysql_query($query, $db_connection);
+                if(!rs) {
+                  die("Query failed: " . mysql_error());
+                }
+                else {
+                  echo "Successfully insert into database.";
+
+                  // update maxperson id
+                  mysql_query("UPDATE MaxPersonID SET id=" . $new_id, $db_connection);
+                }
+              }
+              mysql_close($db_connection);
+            }
+          ?>
+        </div>
+      </div>
+    </div>
+  </body>
 </html>
--- Project 1B/www/add_movie.php
+++ Project 1B/www/add_movie.php
@@ -16,139 +16,159 @@
   <!-- Latest compiled and minified JavaScript -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </head>
-<body>
-   <!-- Navbar -->
+  <body>
+    <!-- Navbar -->
     <div class="navbar navbar-inverse navbar-static-top center" role="navigation">
-        <div class="container">
+      <div class="container">
 
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-              <a href="index.php">
-                <div class="navbar-brand">
-                  CS 143
-                  <small>Project 1B</small>
-                </div>
-              </a>
-          </div>
-
-          <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Add New Content<b class="caret"></b></a>
-                <ul class="dropdown-menu dropdown-menu-left">
-                  <li><a href="add_actordirector.php">Add Actor/Director</a></li>
-                  <li><a href="add_movie.php">Add Movie Information</a></li>
-                  <li><a href="add_movieactor.php">Add Movie/Actor Relation</a></li>
-                  <li><a href="add_moviedirector.php">Add Movie/Director Relation</a></li>
-                </ul>
-              </li>
-
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Browsing Content<b class="caret"></b></a>
-                <ul class="dropdown-menu dropdown-menu-left">
-                  <li><a href="show_actor.php">Show Actor Information</a></li>
-                  <li><a href="show_movie.php">Show Movie Information</a></li>
-                </ul>
-              </li>
-
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Search Interface<b class="caret"></b></a>
-                <ul class="dropdown-menu dropdown-menu-left">
-                  <li><a href="search.php">Search Actor/Movie</a></li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="index.php">
+            <div class="navbar-brand">
+              CS 143
+              <small>Project 1B</small>
+            </div>
+          </a>
         </div>
-      </div>
-    </nav>
-  <!-- End Navbar -->
 
-  <!-- Section -->
-  <div class="container" style="margin-left: 0">
-    <div class="row">
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Add New Content<b class="caret"></b></a>
+              <ul class="dropdown-menu dropdown-menu-left">
+                <li><a href="add_actordirector.php">Add Actor/Director</a></li>
+                <li><a href="add_movie.php">Add Movie Information</a></li>
+                <li><a href="add_movieactor.php">Add Movie/Actor Relation</a></li>
+                <li><a href="add_moviedirector.php">Add Movie/Director Relation</a></li>
+              </ul>
+            </li>
 
-      <div class="col-md-3 sidebar">
-        <ul class="nav nav-sidebar">
-            <h3>Add New Content</h3>
-            <li><a href="add_actordirector.php">Add Actor/Director</a></li>
-            <li><a href="add_movie.php">Add Movie Information</a></li>
-            <li><a href="add_movieactor.php">Add Movie/Actor Relation</a></li>
-            <li><a href="add_moviedirector.php">Add Movie/Director Relation</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Browsing Content<b class="caret"></b></a>
+              <ul class="dropdown-menu dropdown-menu-left">
+                <li><a href="show_actor.php">Show Actor Information</a></li>
+                <li><a href="show_movie.php">Show Movie Information</a></li>
+              </ul>
+            </li>
+
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Search Interface<b class="caret"></b></a>
+              <ul class="dropdown-menu dropdown-menu-left">
+                <li><a href="search.php">Search Actor/Movie</a></li>
+              </ul>
+            </li>
           </ul>
+        </div>
 
-          <ul class="nav nav-sidebar">
-            <h3>Browsing Content</h3>
-            <li><a href="show_actor.php">Show Actor Information</a></li>
-            <li><a href="show_movie.php">Show Movie Information</a></li>
-          </ul>
-
-          <ul class="nav nav-sidebar">
-            <h3>Search Interface</h3>
-            <li><a href="search.php">Search Actor/Movie</a></li>
-          </ul>
-      </div>
-      <div class="col-md-9">
-        <form action="add_actordirector.php" method="GET" style="margin-left: 5%">
-          <h3>Add New Movie</h3>
-          <div class="form-group">
-             <label for="title">Title</label>
-            <input type="text" class="form-control" placeholder="Text Input" name="title">
-          </div>
-          <div class="form-group">
-             <label for="company">Company</label>
-            <input type="text" class="form-control" placeholder="Text Input" name="company">
-          </div>
-          <div class="form-group">
-             <label for="year">Year</label>
-            <input type="text" class="form-control" placeholder="Text Input" name="year">
-          </div>
-          <div class="form-group">
-             <label for="rating">MPAA Rating</label>
-             <select class="form-control" name="rate">
-              <option value="G">G</option>
-              <option value="NC-17">NC-17</option>
-              <option value="PG">PG</option>
-              <option value="PG-13">PG-13</option>
-              <option value="R">R</option>
-              <option value="surrendere">surrendere</option>
-             </select>
-          </div>
-          <div class="form-group">
-            <label>Genre:</label>
-            <input type="checkbox" name="genre[]" value="Action">Action</input>
-            <input type="checkbox" name="genre[]" value="Adult">Adult</input>
-            <input type="checkbox" name="genre[]" value="Adventure">Adventure</input>
-            <input type="checkbox" name="genre[]" value="Animation">Animation</input>
-            <input type="checkbox" name="genre[]" value="Comedy">Comedy</input>
-            <input type="checkbox" name="genre[]" value="Crime">Crime</input>
-            <input type="checkbox" name="genre[]" value="Documentary">Documentary</input>
-            <input type="checkbox" name="genre[]" value="Drama">Drama</input>
-            <input type="checkbox" name="genre[]" value="Family">Family</input>
-            <input type="checkbox" name="genre[]" value="Fantasy">Fantasy</input>
-            <input type="checkbox" name="genre[]" value="Horror">Horror</input>
-            <input type="checkbox" name="genre[]" value="Musical">Musical</input>
-            <input type="checkbox" name="genre[]" value="Mystery">Mystery</input>
-            <input type="checkbox" name="genre[]" value="Romance">Romance</input>
-            <input type="checkbox" name="genre[]" value="Sci-Fi">Sci-Fi</input>
-            <input type="checkbox" name="genre[]" value="Short">Short</input>
-            <input type="checkbox" name="genre[]" value="Thriller">Thriller</input>
-            <input type="checkbox" name="genre[]" value="War">War</input>
-            <input type="checkbox" name="genre[]" value="Western">Western</input>
-          </div>
-          <button type="submit" class="btn btn-default">Add</button>
-        </form>
       </div>
     </div>
-  </div>
+    <!-- End Navbar -->
 
+    <!-- Section -->
+    <div class="container" style="margin-left: 0">
+      <div class="row">
 
-</body>
+        <div class="col-md-3 sidebar">
+          <ul class="nav nav-sidebar">
+              <h3>Add New Content</h3>
+              <li><a href="add_actordirector.php">Add Actor/Director</a></li>
+              <li><a href="add_movie.php">Add Movie Information</a></li>
+              <li><a href="add_movieactor.php">Add Movie/Actor Relation</a></li>
+              <li><a href="add_moviedirector.php">Add Movie/Director Relation</a></li>
+            </ul>
 
+            <ul class="nav nav-sidebar">
+              <h3>Browsing Content</h3>
+              <li><a href="show_actor.php">Show Actor Information</a></li>
+              <li><a href="show_movie.php">Show Movie Information</a></li>
+            </ul>
+
+            <ul class="nav nav-sidebar">
+              <h3>Search Interface</h3>
+              <li><a href="search.php">Search Actor/Movie</a></li>
+            </ul>
+        </div>
+        <div class="col-md-9">
+          <form action="add_actordirector.php" method="GET" style="margin-left: 5%">
+            <h3>Add New Movie</h3>
+            <div class="form-group">
+              <label for="title">Title</label>
+              <input type="text" class="form-control" placeholder="Text Input" name="title">
+            </div>
+            <div class="form-group">
+              <label for="company">Company</label>
+              <input type="text" class="form-control" placeholder="Text Input" name="company">
+            </div>
+            <div class="form-group">
+              <label for="year">Year</label>
+              <input type="text" class="form-control" placeholder="Text Input" name="year">
+            </div>
+            <div class="form-group">
+              <label for="rating">MPAA Rating</label>
+              <select class="form-control" name="rate">
+                <option value="G">G</option>
+                <option value="NC-17">NC-17</option>
+                <option value="PG">PG</option>
+                <option value="PG-13">PG-13</option>
+                <option value="R">R</option>
+                <option value="surrendere">surrendere</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label>Genre:</label>
+              <?php
+                $db_connection = mysql_connect("localhost", "cs143", "");
+                mysql_select_db("CS143", $db_connection);
+
+                $genre_query = mysql_query("SELECT DISTINCT genre FROM MovieGenre ORDER BY genre", $db_connection);
+                if(!$genre_query) {
+                  die("Query failed: " . mysql_error());
+                }
+                $genre_count = mysql_num_rows($genre_query);
+
+                while($row = mysql_fetch_assoc($genre_query)){
+                  $genre[] = $row[genre];
+                }
+                for ($i = 0; $i < $genre_count; $i++) {
+                  $cur = $genre[$i];
+                  echo ("<input type='checkbox' name='genre[]' value='" . $cur . "'> " . $cur . "</input> ");
+                }
+
+              ?> 
+            </div>
+            <button type="submit" class="btn btn-default">Add</button>
+          </form>
+          <br>
+          <?php
+            $rs = mysql_query("SELECT id FROM MaxMovieID", $db_connection);
+            if(!$rs) {
+              die("Query failed: " . mysql_error());
+            }
+            $row = mysql_fetch_row($rs);
+            $MaxMovieID = $row[0];
+       
+            // create new unique ID
+            $new_id = $MaxMovieID + 1;
+
+            // create insertion query
+            $title = $_GET["title"];
+            $company = "\"" . $_GET["company"] . "\"";
+            $year = "\"" . $_GET["year"] . "\"";
+            $rate = "\"" . $_GET["rate"] . "\"";
+            $genre = "\"" . $_GET["genre"] . "\"";
+
+            $query_Movie = "INSERT INTO Movie VALUES(" . $new_id . "," . $title . "," . $year . "," . $rate . "," . $company . ")";
+            foreach ($genre_get as $genre) {
+              $query_MovieGenre = "INSERT INTO MovieGenre VALUES (" . $new_id . "," . $genre . ")";
+            }
+          ?>
+        </div>
+      </div>
+    </div>
+  </body>
 </html>
